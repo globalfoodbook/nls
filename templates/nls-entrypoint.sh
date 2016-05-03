feeb7e5afb8132bdc90db30d3616cc8d66fb71a3
Ikenna Okpala <me@ikennaokpala.com>
2016-05-03 02:56:46 +0100

    # Reusing entrypoint from nls-pg

--- templates/nls-entrypoint.sh
+++ templates/nls-entrypoint.sh
@@ -2,9 +2,5 @@
 
 set -e
 set -x
-
-export PATH=$GOPATH/bin:/usr/local/go/bin:/usr/lib/postgresql/$POSTGRES_VERSION/bin/:$PATH
-export NUT_PG_DSN="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_IP/$POSTGRES_DB?sslmode=disable"
-#!/bin/bash
-
-/etc/init.d/pg_entrypoint.sh $POSTGRES_MAIN_USER
+# Reusing entrypoint from nls-pg
+/etc/init.d/entrypoint.sh
