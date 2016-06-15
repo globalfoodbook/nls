e17b59fae586a62eab668cf2d1e6bd79ecd79b2b
Ikenna Okpala <me@ikennaokpala.com>
2016-06-15 03:00:25 +0100

    Tag docker hub build with version number

--- deploy_to_hub.sh
+++ deploy_to_hub.sh
@@ -4,5 +4,15 @@
 eval "$(docker-machine env default)"
 
 /usr/local/bin/docker login
-/usr/local/bin/docker build -t globalfoodbook/nls:latest .
-/usr/local/bin/docker push globalfoodbook/nls:latest
+
+read -p "What version number or tag are you deploying? " tag
+
+if [[ $tag ]]; then
+  /usr/local/bin/docker build -t globalfoodbook/nls:$tag .
+  /usr/local/bin/docker push globalfoodbook/nls:$tag
+
+  /usr/local/bin/docker build -t globalfoodbook/nls:latest .
+  /usr/local/bin/docker push globalfoodbook/nls:latest
+else
+  echo -e "Kindly provide a tag or version number"
+fi
